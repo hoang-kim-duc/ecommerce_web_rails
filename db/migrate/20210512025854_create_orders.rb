6f76323df499bbb2837092a7fada0ddbb6d91a91
duchk-2510 <hoang.kim.duc@sun-asterisk.com>
2021-05-13 09:52:59 +0700

    list product

--- db/migrate/20210512025854_create_orders.rb
+++ db/migrate/20210512025854_create_orders.rb
@@ -6,10 +6,11 @@ class CreateOrders < ActiveRecord::Migration[6.1]
       t.integer :total
       t.integer :status, default: 0
       t.bigint :user_id, null: false
-      t.bigint :delivery_infomations
+      t.bigint :delivery_infomation_id
 
       t.timestamps
     end
     add_foreign_key :orders, :users
+    add_foreign_key :orders, :delivery_infomation
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.create!(
   password_confirmation: "123456",
   role: 1,
   activated: true,
-  activated_at: Time.zone.now,
+  activated_at: Time.zone.now
 )
 
 30.times do |n|
@@ -18,6 +18,22 @@ User.create!(
     password: password,
     password_confirmation: password,
     activated: true,
-    activated_at: Time.zone.now,
+    activated_at: Time.zone.now
+  )
+end
+
+Category.create!(
+  title: "Demo",
+  father_id: nil
+)
+
+30.times do |n|
+  Product.create!(
+    name: Faker::Device.model_name,
+    manufacturer: Faker::Device.manufacturer,
+    detail: "Product #{n + 1} detail",
+    quantity: Faker::Number.within(range: 5..20),
+    price: Faker::Number.within(range: 10..30) * 1000000,
+    category_id: 1
   )
 end
